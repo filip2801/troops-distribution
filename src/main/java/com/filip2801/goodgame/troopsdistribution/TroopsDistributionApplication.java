9b5e0fb630e377b29ae1741099d17b0749f60d60
Filip Karaś <filip.karas@hawk.ai>
2021-11-28 10:45:46 +0100

    Init project

---
+++ src/main/java/com/filip2801/goodgame/troopsdistribution/TroopsDistributionApplication.java
@@ -0,0 +1,13 @@
+package com.filip2801.goodgame.troopsdistribution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TroopsDistributionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TroopsDistributionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/filip2801/goodgame/troopsdistribution/TroopsDistributionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.filip2801.goodgame.troopsdistribution;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TroopsDistributionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
